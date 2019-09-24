065f907fbc3a2ce491b0c5fe1a19686e55e6719b
NikitaKrivulin <krivulin.nikita@yandex.ru>
2019-09-24 16:12:21 +0300

    watcher added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 let gulp = require('gulp');
 let pug = require('gulp-pug');
+let htmlbeautify = require('gulp-html-beautify');
+let browserSync = require('browser-sync').create();
 
 function buildHtml(cb) {
 
     gulp.src('src/*.pug')
         .pipe(pug())
-        .dest("public");
+        .pipe(htmlbeautify())
+        .pipe(gulp.dest("public"));
 
     cb();
 }
 
+function watch(cb) {
+
+    browserSync.init({
+        server: {
+            baseDir: "public"
+        },
+    });
+
+    gulp.watch([ "src/**.pug" ], buildHtml).on('change', browserSync.reload);
+    cb();
+}
+
+exports.watch = watch;
 exports.default = buildHtml
